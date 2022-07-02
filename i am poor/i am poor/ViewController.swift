1b0a72cb8d3f942aaa2b76403ec98825f6d09a40
Riv <53383837+rivenshell@users.noreply.github.com>
2022-07-02 07:05:23 -0400

    Add files via upload

---
+++ i am poor/i am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am poor
+//
+//  Created by Riv on 7/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
